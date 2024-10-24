5ce5976ecd158b173a65473030950774383baa15
Alex Fila <fialex66@gmail.com>
2024-10-24 12:55:52 +0200

    upload changes

---
+++ Irisproject/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  Irisproject
+//
+//  Created by Alex Fila on 24/10/24.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
---
+++ Irisproject/DatepickerView.swift
@@ -0,0 +1,18 @@
+//
+//  DatepickerView.swift
+//  Irisproject
+//
+//  Created by Alex Fila on 24/10/24.
+//
+
+import SwiftUI
+
+struct DatepickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatepickerView()
+}
