7df46fd872bcc3936eb4af5a4374e31bbec01cdd
Alex Fila <fialex66@gmail.com>
2024-10-24 11:42:25 +0200

    Initial Commit

---
+++ Irisproject/IrisprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrisprojectApp.swift
+//  Irisproject
+//
+//  Created by Alex Fila on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct IrisprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
